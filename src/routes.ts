d60bfde5306cd82c9ab72ab46f68e191e168b403
“julianmendonca” <julianmnendon@gmail.com>
2022-12-06 20:19:40 -0300

    clean

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import DefaultRouter from './domain/default.route';
-
-const Routes = Router();
-
-Routes.use('/', DefaultRouter);
-
-export default Routes;
--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-
-import application from './application';
-
-((): void => {
-  application.listen(3000, (): boolean =>
-    process.stdout.write(`Server running at port http://localhost:${3000}\n`)
-  );
-})();
