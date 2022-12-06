571637d61ae0e72586b9c6b220e1dac59be006af
“julianmendonca” <julianmnendon@gmail.com>
2022-12-06 20:28:48 -0300

    refactor

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { Configuration, OpenAIApi } = require("openai");
+
+const app = express();
+const port = 3000;
+
+const configuration = new Configuration({
+  apiKey:
+    process.env.OPENAI_API_KEY ||
+    "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",
+});
+const openai = new OpenAIApi(configuration);
+
+app.get("/", async (req, res) => {
+  const response = await openai.createImage({
+    prompt: "a white siamese cat",
+    n: 1,
+    size: "1024x1024",
+  });
+  res.send(response.data.data[0].url);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
